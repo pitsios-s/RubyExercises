8533bfb630b3a4363d27f4413e5082ddbc2bf2d0
pitsios-s <stamatis.pit@gmail.com>
2014-05-17 19:33:34 +0300

    Modified file AuebRocks.rb

--- src/AuebRocks.rb
+++ src/AuebRocks.rb
@@ -1,5 +1,13 @@
-#! /usr/bin/ruby
+#!/usr/bin/ruby
+
+#This program asks the user for an integer , and then prints
+#that many times a phrase , unless false.
 
 statement = false
 
-puts "AUEB Rocks!!!" unless statement
+print "Please give an integer : "
+input = Integer(gets.chomp)
+
+input.times do
+   puts "AUEB Rocks!!!" unless statement
+end
